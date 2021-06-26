3a13dd185d6d3b3de7221f5b7f5e5d595a6a0e2b
Brittany Wolff <brittzyx@msn.com>
2021-06-26 14:02:43 -0600

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     end
 
     scope :swab, -> { where(subtitle: "Swab them planks!") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "https://picsum.photos/600/400"
+        self.thumb_image ||= "https://picsum.photos/350/200"
+    end
 end
