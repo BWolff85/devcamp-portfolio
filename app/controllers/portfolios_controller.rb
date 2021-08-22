8043a7efdd6c4e8b22e3f8b3d1c208e5918d1814
Brittany Wolff <brittzyx@msn.com>
2021-08-22 11:34:41 -0600

    added custom layouts for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,60 +1,62 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
-    end
+  layout "portfolio"
 
-    def swab
-        @swab_portfolio_items = Portfolio.swab
-    end
+  def index
+      @portfolio_items = Portfolio.all
+  end
 
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
-    end
+  def swab
+      @swab_portfolio_items = Portfolio.swab
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build }
-    end
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-    def create
-        @portfolio_item = Portfolio.new(portfolio_item_params)
+  def new
+      @portfolio_item = Portfolio.new
+      3.times { @portfolio_item.technologies.build }
+  end
 
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: "Boom! That worked." }
-          else
-            format.html { render :new, status: :unprocessable_entity }
-          end
-        end
-    end
+  def create
+      @portfolio_item = Portfolio.new(portfolio_item_params)
 
-    def edit
-        @portfolio_item = Portfolio.find(params[:id])
-    end
-
-    def update
-      @portfolio_item = Portfolio.find(params[:id])
       respond_to do |format|
-        if @portfolio_item.update(portfolio_item_params)
-          format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: "Boom! That worked." }
         else
-          format.html { render :edit, status: :unprocessable_entity }
+          format.html { render :new, status: :unprocessable_entity }
         end
       end
-    end
+  end
 
-    def destroy
-      #perform lookup
+  def edit
       @portfolio_item = Portfolio.find(params[:id])
-      
-      #destroy the record
-      @portfolio_item.destroy
-      
-      #redirect
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+    #perform lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    #destroy the record
+    @portfolio_item.destroy
+    
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+    end
+  end
     
   private
   # Use callbacks to share common setup or constraints between actions.
